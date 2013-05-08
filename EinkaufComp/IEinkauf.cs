10529231424318c9b59b7f6ca56c0829af1a55fe
Jan Menzel <jan.menzel@haw-hamburg.de>
2013-05-08 20:28:44 +0200

    LagerComp erstellt, EinkaufsComp erstellt

---
+++ EinkaufComp/IEinkauf.cs
@@ -0,0 +1,17 @@
+﻿using EinkaufComp.Lieferant.Repository.Entity;
+using LagerComp.Produkt.Repository.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EinkaufComp
+{
+    public interface IEinkauf
+    {
+        void bestelleProdukt(ProduktNummerTyp prod, LieferantNummerTyp lieferant, int Anzahl);
+        void addProduktVerfuegbarCallback(ProduktNummerTyp prod, Callbacks.ProduktVerfuegbar cb);
+
+    }
+}
---
+++ LagerComp/ILager.cs
@@ -0,0 +1,18 @@
+﻿using LagerComp.Produkt.Repository.Entity;
+using LagerComp.Repository.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagerComp
+{
+    public interface ILager
+    {
+        MeldungsNummerTyp erstelleWarenausgang(ProduktNummerTyp prod, int Anzahl);
+        MeldungsNummerTyp erstelleWareneingang(ProduktNummerTyp prod, int Anzahl);
+        bool pruefeLagerbestand(ProduktNummerTyp prod, int Anzahl);
+        ProduktNummerTyp erstelleProdukt(ProduktDetailsTyp prod);
+    }
+}
