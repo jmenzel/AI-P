8ade51f83df6a129b24f379bfbbe240b82c6eb64
bDino <b.dino@arcor.de>
2013-05-08 22:51:07 +0200

    Auftragserfassung angepasst

--- AuftragserfassungComp/Logic/AngebotTyp.cs
+++ AuftragserfassungComp/Logic/AngebotTyp.cs
@@ -8,5 +8,24 @@ namespace HES.AuftragserfassungComp.Logic
 {
     class AngebotTyp : IAuftragserfassung
     {
+        public Repository.Entity.AngebotNummerTyp erstelleAngebot(Repository.Entity.AngebotTyp angebot, Kunde.Repository.Entity.KundenNrTyp kunde)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Repository.Entity.AngebotTyp holeAngebot(Repository.Entity.AngebotNummerTyp nr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Repository.Entity.AuftragNummerTyp erstelleAuftrag(Repository.Entity.AngebotNummerTyp nr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void markiereAuftrag(Repository.Entity.AuftragNummerTyp auftrag, Repository.Entity.AuftragStatusTyp status)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
