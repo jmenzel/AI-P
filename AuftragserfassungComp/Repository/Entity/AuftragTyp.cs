3a8a1f64d5e16d2b99c6beb90b25ffb54e39e438
Jan Menzel <jan.menzel@haw-hamburg.de>
2013-05-10 21:41:01 +0200

    Vorhandene Komponenten erweitert, Rechnungs Komponente hinzuefgt

--- AuftragserfassungComp/Repository/Entity/AuftragTyp.cs
+++ AuftragserfassungComp/Repository/Entity/AuftragTyp.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HES.AuftragserfassungComp.Repository.Entity
 {
-    class AuftragTyp
+    public class AuftragTyp
     {
     }
 }
---
+++ RechnungComp/IRechnung.cs
@@ -0,0 +1,16 @@
+﻿using HES.AuftragserfassungComp.Repository.Entity;
+using HES.Kunde.Repository.Entity;
+using RechnungComp.Repository.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RechnungComp
+{
+    public interface IRechnung
+    {
+        RechnungsNrTyp erstelleRechnung(KundenNrTyp kndNr, AuftragTyp auftrag);
+    }
+}
