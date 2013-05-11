1fb5cc87b29b3f2aa3d372a10a74bf88f7c11e83
Jan Menzel <jan.menzel@haw-hamburg.de>
2013-05-11 12:30:01 +0200

    Kunden Mapping geadded (unfertig)

--- HESCore/Mappings/KundeMap.cs
+++ HESCore/Mappings/KundeMap.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FluentNHibernate.Mapping;
+using HES.Kunde.Repository.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,29 @@ using System.Threading.Tasks;
 
 namespace HES.Mappings
 {
-    public class KundeMap
+
+    public class KundenNrMapping : ClassMap<KundenNrTyp>
     {
-        
+        public KundenNrMapping()
+        {
+            Id(x => x.value);
+        }
+    }
+
+    public class KundeMap : ClassMap<KundeTyp>
+    {
+        public KundeMap()
+        {
+            Id(x => x.kundenNr);
+            Map(x => x.level);
+            Map(x => x.vorname);
+            Map(x => x.name);
+            Map(x => x.geburtstag);
+            Map(x => x.hausnr);
+            Map(x => x.strasse);
+            Map(x => x.ort);
+            Map(x => x.plz);
+            Map(x => x.land);
+        }
     }
 }
