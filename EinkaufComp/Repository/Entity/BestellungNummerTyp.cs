05bd58f80d270212baefece0aafb879b9101d0c3
Jan Menzel <jan.menzel@haw-hamburg.de>
2013-05-11 14:56:29 +0200

    EinkaufComp erweitert

---
+++ EinkaufComp/Repository/Entity/BestellungNummerTyp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EinkaufComp.Repository.Entity
+{
+    public class BestellungNummerTyp
+    {
+        public virtual string nr { get; protected set; }
+
+        public BestellungNummerTyp(string nr)
+        {
+            this.nr = nr;
+        }
+
+        protected BestellungNummerTyp() { }
+    }
+}
---
+++ EinkaufComp/Repository/Entity/BestellungTyp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EinkaufComp.Repository.Entity
+{
+    public class BestellungTyp
+    {
+        public virtual BestellungNummerTyp nr { get; protected set; }
+        public virtual DateTime Bestelldatum { get; protected set; }
+        public virtual int Menge { get; protected set; }
+        public virtual bool Freigabe { get; protected set; }
+
+        public BestellungTyp(BestellungNummerTyp nr, DateTime datum, int Menge, bool Freigabe)
+        {
+            this.nr = nr;
+            this.Bestelldatum = datum;
+            this.Menge = Menge;
+            this.Freigabe = Freigabe;
+        }
+
+        protected BestellungTyp() { }
+    }
+}
